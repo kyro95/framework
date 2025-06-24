e93fdce13fd250c270ce5d401006475064f48edc
Neeko <gtaneeko@gmail.com>
2025-06-24 20:20:54 +0200

    chore(core): clean webview.interface.ts

--- packages/core/src/interfaces/webview.interface.ts
+++ packages/core/src/interfaces/webview.interface.ts
@@ -8,26 +8,8 @@ export interface IWebView {
     on(eventName: string, listener: (...args: unknown[]) => void): void;
     emit(eventName: string, ...args: unknown[]): void;
 
+    // ToDo
     //destroy(): void;
     //focus?(): void;
     //unfocus?(): void;
 }
-
-/*export interface IWebView {
-    readonly id: string | number;
-    url: string;
-
-    destroy(): void;
-
-    on(eventName: string, listener: (...args: any[]) => void): void;
-    emit(eventName: string, ...args: any[]): void;
-
-    focus(): void;
-    unfocus(): void;
-
-    //id: string | number;
-    // on(event: string, listener: (...args: unknown[]) => void): void;
-    //on(eventName: string, listener: (...args: unknown[]) => void): void;
-    //emit(event: string, ...args: unknown[]): void;
-    destroy(): void;
-}*/
