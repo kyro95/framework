7987c47721633496e664da8e532d09925e4088a4
NeekoGta <gtaneeko@gmail.com>
2025-06-26 16:05:52 +0200

    chore(core): update lifecycle hooks.

--- packages/core/src/interfaces/lifecycle-hooks.interface.ts
+++ packages/core/src/interfaces/lifecycle-hooks.interface.ts
@@ -1,21 +1,25 @@
 /**
- * Interface for a class that wishes to run logic after the entire application has been bootstrapped.
+ * Interface for a class that wishes to run logic after the application has been initialized.
  */
-export interface OnApplicationBootstrap {
-    /**
-     * Method called once the application has fully started.
-     */
-    onApplicationBootstrap(): void;
+export interface OnAppInit {
+    onAppInit(): void;
+}
+
+/**
+ * Interface for a class that wishes to run logic after the application has start.
+ */
+export interface OnAppStarted {
+    onAppStarted(): void;
 }
 
 /**
  * Interface for a class that wishes to run logic when the application is shutting down.
  * Useful for cleanup tasks like closing database connections.
  */
-export interface OnApplicationShutdown {
+export interface OnAppShutdown {
     /**
      * Method called when the application receives a shutdown signal.
      * @param signal The signal that triggered the shutdown (e.g., 'SIGINT').
      */
-    onApplicationShutdown(signal?: string): void;
+    onAppShutdown(signal?: string): void;
 }
